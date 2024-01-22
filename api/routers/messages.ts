f51a220569b78929b7fe9e3918cca8912cdb4231
Nurzat Taalaibekova <tnurzat91@gmail.com>
2024-01-22 09:34:50 +0300

    error handling when sending

--- api/routers/messages.ts
+++ api/routers/messages.ts
@@ -14,6 +14,9 @@ messagesRouter.post("/", async (req, res) => {
     author: req.body.author,
     message: req.body.message,
   };
+  if(!messages) {
+    return res.status(422).send({error: 'Author and message must be present in the request'});
+  }
   const savedMessages = await fileDb.addItem(messages);
   res.send(savedMessages);
 });
